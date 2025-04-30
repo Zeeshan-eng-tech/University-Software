4eb58e31f6f9a76b600cbd2e830a2706f22699c8
Zeeshan-eng-tech <zr0134146@gmail.com>
2025-04-30 03:12:29 -0700

    Update Header.js

--- src/app/Header.js
+++ src/app/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link href="/" className="navbar-brand">
-          Hello Students this is your dream site for education.
+          Hello Students this is your dream educational place.
         </Link>
         <button
           className="navbar-toggler"
